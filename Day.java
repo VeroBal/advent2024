ea559fb7eff5b47ab4c2eb3cf18ad514bd763d87
Veronica <veronica.baldin@nexiona.com>
2024-12-01 21:02:30 +0100

    General get input class

---
+++ Day.java
@@ -0,0 +1,38 @@
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Day {
+    String day;
+
+    Day(String dayNumber) {
+        this.day = dayNumber;
+    }
+
+    public ArrayList<String> getInput() throws IOException {
+        String s;
+        String filePath = this.day + "/input.txt";
+        BufferedReader reader = new BufferedReader(new FileReader(filePath));
+        ArrayList<String> lines = new ArrayList<>();
+        while ((s = reader.readLine()) != null) {
+            lines.add(s);
+        }
+        reader.close();
+        return lines;
+    }
+
+    public static void main(String[] args) throws IOException {
+        if (args.length < 1) {
+            System.out.println("Usage: java Day <dayNumber>");
+            return;
+        }
+
+        String dayNumber = args[0];
+        Day day = new Day(dayNumber);
+        ArrayList<String> lines = day.getInput();
+        String firstLine = lines.getFirst();
+        System.out.print(firstLine);
+    }
+}
