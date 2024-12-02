bdf1768f22c6f5195ab2a1b752d63d7a2795749d
Veronica <veronica.baldin@nexiona.com>
2024-12-02 20:09:57 +0100

    Start Day 2

---
+++ d02/Day02Solver.java
@@ -0,0 +1,19 @@
+package d02;
+
+import java.util.ArrayList;
+
+import advent.Solver;
+
+public class Day02Solver extends Solver {
+    public Day02Solver() {
+        super();
+    }
+
+    @Override
+    public void solve1(ArrayList<String> lines) {
+    }
+
+    @Override
+    public void solve2(ArrayList<String> lines) {
+    }
+}
