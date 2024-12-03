83fc4e47d192a9d52c30c9d86bd8fd90ce0c2c9c
Veronica <veronica.baldin@nexiona.com>
2024-12-03 09:15:07 +0100

    Part 2

--- d02/Day02Solver.java
+++ d02/Day02Solver.java
@@ -1,6 +1,7 @@
 package d02;
 
 import java.util.ArrayList;
+import java.util.Optional;
 
 import advent.Solver;
 
@@ -9,7 +10,7 @@ public class Day02Solver extends Solver {
         super();
     }
 
-    public ArrayList<Integer> parseLine(String line){
+    public ArrayList<Integer> parseLine(String line) {
         ArrayList<Integer> levels = new ArrayList<>();
         String[] parts = line.split("\\s+");
         for (String part : parts) {
@@ -18,30 +19,56 @@ public class Day02Solver extends Solver {
         return levels;
     }
 
+    private static class Pair {
+        private final int first;
+        private final int second;
+
+        public Pair(int first, int second) {
+            this.first = first;
+            this.second = second;
+        }
+
+        public int getFirst() {
+            return first;
+        }
+
+        public int getSecond() {
+            return second;
+        }
+
+        @Override
+        public String toString() {
+            return "(" + first + ", " + second + ")";
+        }
+    }
+
+    public Optional<Pair> unsafeLevel(ArrayList<Integer> levels) {
+        boolean isIncreasing = true;
+        for (int i = 1; i < levels.size(); i++) {
+            int j = i - 1;
+            int difference = levels.get(i) - levels.get(j);
+            if (difference == 0 || difference > 3 || difference < -3) {
+                Pair unsafeLevels = new Pair(i, j);
+                return Optional.of(unsafeLevels);
+            }
+            boolean currentIncreasing = difference > 0;
+            if (i == 1) {
+                isIncreasing = currentIncreasing;
+            } else if (isIncreasing != currentIncreasing) {
+                Pair unsafeLevels = new Pair(i, j);
+                return Optional.of(unsafeLevels);
+            }
+        }
+        return Optional.empty();
+    }
+
     @Override
     public void solve1(ArrayList<String> lines) {
         int safeLevels = 0;
-        for(String line: lines){
+        for (String line : lines) {
             ArrayList<Integer> levels = parseLine(line);
-            boolean isIncreasing = true;
-            boolean isSafe = true;
-            for(int i=1; i<levels.size(); i++){
-                int j=i-1;
-                int difference = levels.get(i) - levels.get(j);
-                if(difference == 0 || difference > 3 || difference < -3){
-                    isSafe = false;
-                    break;
-                } 
-                boolean currentIncreasing = difference > 0;
-                if(i == 1){
-                    isIncreasing = currentIncreasing;
-                }
-                else if(isIncreasing != currentIncreasing){
-                    isSafe = false;
-                    break;
-                }
-            }
-            if(isSafe){
+            boolean isSafe = unsafeLevel(levels).isEmpty();
+            if (isSafe) {
                 safeLevels++;
             }
         }
@@ -51,5 +78,33 @@ public class Day02Solver extends Solver {
 
     @Override
     public void solve2(ArrayList<String> lines) {
+        int safeLevels = 0;
+        for (String line : lines) {
+            ArrayList<Integer> levels = parseLine(line);
+
+            // First, check if the original sequence is already safe
+            if (unsafeLevel(levels).isEmpty()) {
+                safeLevels++;
+                continue;
+            }
+
+            // Try removing each level one by one
+            boolean foundSafeRemoval = false;
+            for (int i = 0; i < levels.size(); i++) {
+                ArrayList<Integer> listCopy = new ArrayList<>(levels);
+                listCopy.remove(i);
+
+                if (unsafeLevel(listCopy).isEmpty()) {
+                    foundSafeRemoval = true;
+                    break;
+                }
+            }
+
+            if (foundSafeRemoval) {
+                safeLevels++;
+            }
+        }
+
+        System.out.println(safeLevels);
     }
 }
